436f12212e57ab2f6c28d9024e106f50108385f4
Alexander Kozma <alexanders.kozma@gmail.com>
2019-05-23 09:44:08 -0700

    restructuring

---
+++ scripts/global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var		server_selection	= '127.0.0.1'
+var		player_id			= 0
+var		player_name
+var		ui_mode				= false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
