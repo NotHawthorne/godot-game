e61f23d856ff37aad60af83d94ee8295a5e78bb2
Cameron <mueslimak3r@users.noreply.github.com>
2019-05-23 14:15:56 -0700

    fixed preview viewport

--- scenes/objects/UI/VR-Viewport-UI.gd
+++ scenes/objects/UI/VR-Viewport-UI.gd
@@ -1,6 +1,7 @@
 extends Viewport
 
 export var target_fps = 30
+var simulate_fps = 0.0
 
 func set_viewport_texture(p_texture):
 	$"VR-Preview".texture = p_texture
@@ -12,3 +13,14 @@ func _ready():
 
 func _on_resize():
 	size = OS.get_window_size()
+
+func _process(delta):
+	simulate_fps += delta
+	if simulate_fps > (1.0 / target_fps):
+		simulate_fps = 0.0
+		render_target_update_mode = Viewport.UPDATE_ONCE
+		
+		var texture_size = $"VR-Preview".texture.get_size()
+		var texture_scale = size.x / texture_size.x
+		$"VR-Preview".rect_size = texture_size * texture_scale
+		$"VR-Preview".rect_position = (size - $"VR-Preview".rect_size) * 0.5
