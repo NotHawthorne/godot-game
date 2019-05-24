b5fe795d0c3c055c28af6ba0648be9e7c41a2fe7
Alexander Kozma <alexanders.kozma@gmail.com>
2019-05-24 03:58:06 -0700

    experimental change to server

--- scripts/network/network.gd
+++ scripts/network/network.gd
@@ -60,9 +60,9 @@ func			_server_disconnected():
 
 remote	func	register_new_player(id, name):
 	if get_tree().is_network_server():
-		rpc_id(id, "register_new_player", 1, player_name)
+		# rpc_id(id, "register_new_player", 1, player_name)
 		for peer_id in players:
-			rpc_id(id, "register_new_player", peer_id, players[peer_id])
+			rpc_id(peer_id, "register_new_player", id, name)
 	players[id] = name
 	spawn_player(id, name)
 
