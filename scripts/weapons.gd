64234884dd3d4f02700b13455fa47c54f1d9c921
Alexander Kozma <alexanders.kozma@gmail.com>
2019-05-26 11:23:17 -0700

    models! weapons!

---
+++ scripts/weapons.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var	pistol = {"damage": 20, "cooldown": 500, "model": load("res://models/pistol.tscn")}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
