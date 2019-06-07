489380a8871fad2dcb7d748230b9f82e71382d6c
Cameron <cameronlambert98@gmail.com>
2019-06-06 23:21:18 -0700

    fixed leaks on pickups

--- scenes/objects/Objects/Ammo_pickup/Ammo_pickup.gd
+++ scenes/objects/Objects/Ammo_pickup/Ammo_pickup.gd
@@ -9,29 +9,24 @@ var cooldown		= Timer.new()
 var model = load("res://scenes/objects/Objects/Ammo_pickup/Ammo_Capsule.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	capsule = model.instance()
-	self.add_child(capsule)
 	cooldown.set_one_shot(true)
 	cooldown.set_wait_time(cooldown_time)
 	cooldown.connect("timeout", self, "flip_cooldown")
 	add_child(cooldown)
-	capsule.connect("body_entered", self, "collided")
-
-func flip_cooldown():
-	print("capsule regen")
-	self.add_child(capsule)
 	capsule = model.instance()
 	self.add_child(capsule)
 	capsule.connect("body_entered", self, "collided")
 
+func flip_cooldown():
+	self.add_child(capsule)
+
 func collided(body):
 	if body and "ammo" in body :
 		if body.ammo + ammo_to_add > body.max_ammo :
-			body.ammo = body.max_ammo
+			body.amo = body.max_ammo
 		else :
 			body.ammo += ammo_to_add
-		remove_child(capsule)
-		capsule = null
+		self.remove_child(capsule)
 		cooldown.start()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
