069e050ff7519b936648a8ad182d2e3f2fb80759
Cameron <cameronlambert98@gmail.com>
2019-06-13 17:17:39 -0700

    fixed flag spawning

--- scenes/objects/Objects/Flags/Red_Flag_Pad.gd
+++ scenes/objects/Objects/Flags/Red_Flag_Pad.gd
@@ -4,33 +4,45 @@ extends Area
 # var b = "text"
 var capsule
 var model = load("res://scenes/objects/Objects/Flags/Flag.tscn")
+var picked_up = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	capsule = model.instance()
 	self.add_child(capsule)
-	capsule.connect("body_entered", self, "collided")
+	capsule.connect("body_entered", self, "capsule_collided")
+	self.connect("body_entered", self, "pad_collided")
 
 remote func reset_flag() :
 	print("resetting flag on " + global.player_name + "'s client")
 	if self.has_node("Flag") :
 		return
 	self.add_child(capsule)
+	self.get_node("Flag").set_global_transform(self.get_global_transform())
 
 remote func pop_flag() :
 	get_parent().get_node("Red_Flag_Pad").remove_child(capsule)
 
-func collided(body):
-		if "has_flag" in body and body.team == "blue" :
-			if global.player_id == 1 :
-				global.player.pickup_flag(body.player_id, "red")
-			else :
-				global.player.rpc_id(1, "pickup_flag", body.player_id, "red")
-		elif "has_flag" in body and body.has_flag == "red" and body.team == "red" :
+func capsule_collided(body):
+	print("colliding with blue flag")
+	if "has_flag" in body and body.team == "blue" :
+		if global.player_id == 1 :
+			global.player.pickup_flag(body.player_id, "red")
+		else :
+			global.player.rpc_id(1, "pickup_flag", body.player_id, "red")
+	if picked_up == true :
+		if "has_flag" in body and body.team == "red" :
 			if global.player_id == 1 :
 				global.player.reset_flag(body.player_id, "red")
 			else :
 				global.player.rpc_id(1, "reset_flag", body.player_id, "red")
-		elif "has_flag" in body and body.has_flag == "blue" and body.team == "red" :
+
+remote func drop_flag(location) :
+	self.add_child(capsule)
+	self.get_node("Flag").set_global_transform(location)
+
+func pad_collided(body) :
+	if picked_up == false :
+		if "has_flag" in body and body.has_flag == "blue" and body.team == "red" :
 			if global.player_id == 1 :
 				global.player.reset_flag(body.player_id, "blue")
 				global.player.leaderboard_add_stat(body.player_id, 0, 0, 1)
