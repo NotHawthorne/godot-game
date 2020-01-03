82a8a14c2912fd000d085ca0c687f3d092828d14
Cameron <cameronlambert98@gmail.com>
2020-01-03 02:05:46 -0800

    vr movement fixes

--- scenes/objects/VR-Player/VR-Player_helper.gd
+++ scenes/objects/VR-Player/VR-Player_helper.gd
@@ -40,6 +40,7 @@ var		bone_transform
 var		last_do_move	= null
 var		packet_id_cache = {}
 var		first_load		= true
+var		camera_angle	= 0
 const GRAVITY = 9.8
 const JUMP_SPEED = 5800
 const DASH_SPEED = 150
@@ -117,6 +118,7 @@ func _ready():
 		#$"Head/Viewport-VR/ARVROrigin".set_global_transform(self.get_global_transform())
 	global.first_load = false
 	first_load = false
+	camera_angle = $"Head/Viewport-VR/ARVROrigin/ARVRCamera".get_rotation_degrees().y
 	bone_transform = $xbot/Skeleton.get_bone_custom_pose($xbot/Skeleton.find_bone("mixamorig_Spine1"))
 
 func	_on_button_pressed(p_button):
@@ -163,7 +165,6 @@ remote func drop_flag(id, flag_dict, location) :
 			rpc_id(p, "set_flag_owner", id, "blue")
 	if flag_dict["red"] :
 		set_flag_owner(id, "red")
-		location.x = location.transform.basis.x - 1
 		get_parent().get_node("Red_Flag_Pad").drop_flag(location)
 		for p in get_parent().get_node("network").players :
 			get_parent().get_node("Red_Flag_Pad").rpc_id(p, "drop_flag", location)
@@ -314,25 +315,23 @@ func	_physics_process(delta):
 		elif !$JumpCast.is_colliding() and respawning == false :
 			time_off_ground += delta * 2
 		var aim		= $"Head/Viewport-VR/ARVROrigin/ARVRCamera".get_global_transform().basis
-		if Input.is_action_pressed("move_forward"):
-			direction -= aim.z
-			if (anim != "rifle_jump2"):
-				anim = "rifle_run_forward"
-			if time_off_ground == 0 :
-				play_sound("player", player_name, "start", "walk")
+		
 		if $JumpCast.is_colliding() :
-			if Input.is_action_pressed("move_backward"):
+			if Input.is_action_pressed("move_forward"):
 				direction += aim.z
+				#if (anim != "rifle_jump2"):
+				anim = "rifle_run_forward"
+			
+			if Input.is_action_pressed("move_backward"):
+				direction -= aim.z
+				anim = "rifle_run_forward"
 			if Input.is_action_pressed("move_left"):
 				direction -= aim.x
+				anim = "rifle_run_forward"
 			if Input.is_action_pressed("move_right"):
 				direction += aim.x
-			
-			if Input.is_action_pressed("move_backward") and not (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_forward")):
-				play_sound("player", player_name, "start", "walk")
-			if Input.is_action_pressed("move_left") and not (Input.is_action_pressed("move_forward") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_backward")):
-				play_sound("player", player_name, "start", "walk")
-			if Input.is_action_pressed("move_right") and not (Input.is_action_pressed("move_forward") or Input.is_action_pressed("move_left") or Input.is_action_pressed("move_backward")):
+				anim = "rifle_run_forward"
+			if time_off_ground == 0 :
 				play_sound("player", player_name, "start", "walk")
 		else :
 			play_sound("player", player_name, "stop", "walk")
@@ -357,19 +356,19 @@ func	_physics_process(delta):
 					print("colliding")
 					dashing = true
 					velocity.y += (JUMP_SPEED * delta) / 1.9
-					velocity -= aim.z * (DASH_SPEED)
+					velocity -= -aim.z * (DASH_SPEED)
 				if $Head/Camera/WallCast1.is_colliding() and Input.is_action_pressed("move_left"):
 					jumps = 0
 					velocity -= aim.x * (DASH_SPEED * 1.2)
 				if $Head/Camera/WallCast2.is_colliding() and Input.is_action_pressed("move_backward"):
 					jumps = 0
-					velocity += aim.z * (DASH_SPEED * 1.2)
+					velocity += -aim.z * (DASH_SPEED * 1.2)
 				if $Head/Camera/WallCast3.is_colliding() and Input.is_action_pressed("move_right"):
 					jumps = 0
 					velocity += aim.x * (DASH_SPEED * 1.2)
 				if $Head/Camera/WallCast4.is_colliding() and Input.is_action_pressed("move_forward"):
 					jumps = 0
-					velocity -= aim.z * (DASH_SPEED * 1.2)
+					velocity -= -aim.z * (DASH_SPEED * 1.2)
 			if $JumpCast.is_colliding():
 				jumps = 0
 				#print("colliding")
@@ -382,11 +381,11 @@ func	_physics_process(delta):
 			if jumps == 2:
 				if (Input.is_action_pressed("move_forward")):
 					play_sound("player", player_name, "play", "dash")
-					velocity -= aim.z * DASH_SPEED
+					velocity -= -aim.z * DASH_SPEED
 					dashing = true
 				if (Input.is_action_pressed("move_backward")):
 					play_sound("player", player_name, "play", "dash")
-					velocity += aim.z * DASH_SPEED
+					velocity += -aim.z * DASH_SPEED
 					dashing = true
 				if (Input.is_action_pressed("move_left")):
 					play_sound("player", player_name, "play", "dash")
@@ -410,6 +409,20 @@ func	_physics_process(delta):
 		else:
 			accel = RUN_ACCEL * delta
 		velocity = velocity.linear_interpolate(target, accel)
+		var rot_x = -($"Head/Viewport-VR/ARVROrigin/ARVRCamera".get_rotation_degrees().x)
+		var rot_y = 180 + ($"Head/Viewport-VR/ARVROrigin/ARVRCamera".get_rotation_degrees().y)
+		$Head.set_rotation_degrees(Vector3(0, rot_y, 0))
+		$xbot.set_rotation_degrees(Vector3(0, rot_y, 0))
+		#var bone = $xbot/Skeleton.find_bone('mixamorig_Spine')
+		#if rot_x < 90 and rot_x > -90 :
+		#	$Head.set_rotation_degrees(Vector3(rot_x, 0, 0))
+	#		bone_transform = bone_transform.rotated(Vector3(1, 0, 0).normalized(), (-rot_amt) * 0.25)
+		#	bone_transform = bone_transform.rotated(Vector3(0, 0, 1).normalized(), (rot_amt) * 0.25)
+	#		$xbot/Skeleton.set_bone_custom_pose(bone, bone_transform);
+		#rpc_unreliable("do_rot", $xbot.rotation.y, bone_transform, global.player_id)
+
+			#var bone_transform = $xbot/Skeleton.get_bone_custom_pose(bone)
+
 		rpc_unreliable("do_move", velocity, player_id, randi())
 		move_and_slide(velocity, Vector3( 0, 0, 0 ), false, 4, 1, true)
 		var pos = self.get_global_transform()
@@ -479,10 +492,10 @@ remote	func	fire_bullet(id, amt, target):
 		bullet.bullet_owner = id
 		bullet.target = target
 		bullet.set_damage(amt)
-		#if (pnode.vr_player == true):
-		#	pnode.find_node('RayCast', true, false).add_child(bullet)
-		#else:
-		pnode.find_node('gun_container', true, false).add_child(bullet)
+		if (pnode.vr_player == true):
+			pnode.get_node('Head/Viewport-VR/ARVROrigin/Right_Hand/gun_container', true, false).add_child(bullet)
+		else:
+			pnode.find_node('gun_container', true, false).add_child(bullet)
 
 remote	func	set_weapon(id, wid):
 	var pnode = get_parent().get_node(str(id))
