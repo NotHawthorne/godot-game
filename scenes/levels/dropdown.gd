f0238528161eaebcb7ebc0402ff6006d81d02a4d
Cameron <cameronlambert98@gmail.com>
2020-01-09 08:01:45 -0800

    fixed lobby map selection

--- scenes/levels/dropdown.gd
+++ scenes/levels/dropdown.gd
@@ -4,15 +4,16 @@ export (NodePath) var dropdown_path
 onready var dropdown = get_node(dropdown_path)
 
 func _ready():
-	dropdown.add_item("level1")
+	dropdown.add_item("block fortress")
 	dropdown.add_separator()
-	dropdown.add_item("level2")
+	dropdown.add_item("space station")
 	global.lobby_map_selection = "res://scenes/levels/default_level.tscn"
 	dropdown.connect("item_selected", self, "on_item_selected")
 
 func on_item_selected(id):
-	print("selecting item ")
+	
 	if str(dropdown.get_item_text(id)) == "block fortress" :
 		global.lobby_map_selection = "res://scenes/levels/default_level.tscn"
 	elif str(dropdown.get_item_text(id)) == "space station" :
 		global.lobby_map_selection = "res://scenes/levels/space_station.tscn"
+	print("selected map: " + global.lobby_map_selection)
