bc369b860955042a95075086e088a6837d6a875c
Cameron <cameronlambert98@gmail.com>
2020-01-15 13:39:25 -0800

    virtual keyboard wip

--- scripts/ui/input_ip.gd
+++ scripts/ui/input_ip.gd
@@ -9,12 +9,16 @@ func _ready():
 	#var root = get_parent().get_parent()
 	#var http = root.get_node('HTTPRequest')
 	#http.request("http://35.236.33.159:3000/ips.json")
+	connect("focus_entered", self, "_show_keyboard")
 	global.server_selection = '0.0.0.0'
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _show_keyboard() :
+	if OS.has_virtual_keyboard() :
+		OS.show_virtual_keyboard(self.Text)
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
